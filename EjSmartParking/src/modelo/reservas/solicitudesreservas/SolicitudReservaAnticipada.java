2c5e28ca1798ac5ec85aab13df07b46f5ab7786a
IA IA AA YE YE YE YE <alejandro.gallegogragera@alumnos.upm.es>
2026-05-01 23:22:50 +0200

    Enhance SolicitudReservaAnticipada with priority and methods
    Added priority handling and request management methods.

--- EjSmartParking/src/modelo/reservas/solicitudesreservas/SolicitudReservaAnticipada.java
+++ EjSmartParking/src/modelo/reservas/solicitudesreservas/SolicitudReservaAnticipada.java
@@ -3,7 +3,7 @@ package modelo.reservas.solicitudesreservas;
 import java.time.LocalDateTime;
 
 import modelo.vehiculos.Vehiculo;
-
+import modelo.gestoresplazas.GestorLocalidad;
 public class SolicitudReservaAnticipada extends SolicitudReserva {
 
 private TEnumPrioridad prioridad;
@@ -15,5 +15,24 @@ private TEnumPrioridad prioridad;
 	public SolicitudReservaAnticipada (int i, int j, LocalDateTime tI, LocalDateTime tf, Vehiculo vehiculo, TEnumPrioridad prioridad) {
 		super(i,j,tI,tf,vehiculo);
 		this.prioridad=prioridad;
+	}
+	public TEnumPrioridad getPrioridad() {
+	return prioridad;
+	}
+	
+	@Override
+	public void gestionarSolicitudReserva(GestorLocalidad gestor) {
+	super.gestionarSolicitudReserva(gestor);
+		if(super.getHueco() == null) {
+		SolicitudReservaAnticipada solicitud = new SolicitudReservaAnticipada (super.getIZona(),super.getJZona(), super.getTInicial(), super.getTFinal(), super.getVehiculo(), prioridad);
+		gestor.getGestorZona(getIZona(), getJZona()).meterEnListaEspera(solicitud);
+		gestor.getGestorZona(getIZona(), getJZona()).reservarHueco(getTInicial(), getTFinal());
+		setHueco(solicitud.getHueco());
+		}
+			}
+	
+	@Override
+	public boolean esValida (GestorLocalidad gestorLocalidad) {
+	return super.esValida(gestorLocalidad);
 	}
 }
